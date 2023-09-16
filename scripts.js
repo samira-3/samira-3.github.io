e233df6ef4eaae7a6d6c8326e2940e175618348b
Samira <95975916+samira-3@users.noreply.github.com>
2023-09-16 14:15:14 -0400

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,41 +1,39 @@
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('mouseover', function() {
-        this.style.color = 'yellow';
-    });
-    link.addEventListener('mouseout', function() {
-        this.style.color = 'cyan';
-    });
-});
-
 document.addEventListener("DOMContentLoaded", function() {
-    const schoolHeaders = document.querySelectorAll('.school-header');
+    // Link color change
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('mouseover', function() {
+            this.style.color = 'yellow';
+        });
+        link.addEventListener('mouseout', function() {
+            this.style.color = 'cyan';
+        });
+    });
 
+    // School header toggling
+    const schoolHeaders = document.querySelectorAll('.school-header');
     schoolHeaders.forEach(header => {
         header.addEventListener('click', function() {
-            console.log('Header clicked');  // Test if this is being logged
             const details = this.nextElementSibling;
-            if (details.style.display === 'none' || details.style.display === '') {
-                details.style.display = 'block';
-            } else {
-                details.style.display = 'none';
-            }
+            toggleDetails(details);
         });
     });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
+    // Job header toggling
     const jobHeaders = document.querySelectorAll('.job-header');
-
     jobHeaders.forEach(header => {
+        console.log('Binding click event listener to job header.');  // Testing the binding
         header.addEventListener('click', function() {
-           /* console.log('Header clicked');  *// Test if this is being logged
             const details = this.nextElementSibling;
-            if (details.style.display === 'none' || details.style.display === '') {
-                details.style.display = 'block';
-            } else {
-                details.style.display = 'none';
-            }
+            toggleDetails(details);
         });
     });
 });
 
+// A function to toggle the display of details
+function toggleDetails(detailsElement) {
+    if (detailsElement.style.display === 'none' || detailsElement.style.display === '') {
+        detailsElement.style.display = 'block';
+    } else {
+        detailsElement.style.display = 'none';
+    }
+}
