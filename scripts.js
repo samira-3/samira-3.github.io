24f1ea37354fa0cbc0156aef36cc58f790029b00
Samira <95975916+samira-3@users.noreply.github.com>
2023-09-17 03:05:41 -0400

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Link color change
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('mouseover', function() {
+            this.style.color = 'yellow';
+        });
+        link.addEventListener('mouseout', function() {
+            this.style.color = 'green';
+        });
+    });
+
+    function toggleDisplay(element) {
+        element.style.display = element.style.display === 'block' ? 'none' : 'block';
+    }
+
+    function isTouchDevice() {
+        return 'ontouchstart' in window || navigator.maxTouchPoints;
+    }
+
+    function handleEventLogic(headers, details) {
+        headers.forEach((header, index) => {
+            if (isTouchDevice()) {
+                header.addEventListener('click', function() {
+                    toggleDisplay(details[index]);
+                });
+            } else {
+                header.addEventListener('mouseover', function() {
+                    details[index].style.display = 'block';
+                });
+                header.addEventListener('mouseout', function() {
+                    details[index].style.display = 'none';
+                });
+            }
+        });
+    }
+
+    handleEventLogic(document.querySelectorAll('.school-header'), document.querySelectorAll('.school-header + .school-details'));
+    handleEventLogic(document.querySelectorAll('.job-header'), document.querySelectorAll('.job-header + .job-details'));
+});
+
+
+
+
+
+/*document.addEventListener("DOMContentLoaded", function() {
 
     // Link color change
     document.querySelectorAll('a').forEach(link => {
