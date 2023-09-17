bb9e4d1e4c216d5f15a3221323e6db3124c3fcc8
Samira <95975916+samira-3@users.noreply.github.com>
2023-09-17 01:32:30 -0400

    scripts.js

--- scripts.js
+++ scripts.js
@@ -24,45 +24,24 @@ document.addEventListener("DOMContentLoaded", function() {
     handleHoverDisplay('.school-header', '.school-header + .school-details');
     handleHoverDisplay('.job-header', '.job-header + .job-details');
 });
-    
-/*const schoolHeaders = document.querySelectorAll('.school-header');
-const schoolDetails = document.querySelectorAll('.school-header + .school-details');
 
-schoolHeaders.forEach((header, index) => {
-    header.addEventListener('mouseover', function() {
-        schoolDetails[index].style.display = 'block';
-    });
-    header.addEventListener('mouseout', function() {
-        schoolDetails[index].style.display = 'none';
-    });
-});
 
-const jobHeaders = document.querySelectorAll('.job-header');
-const jobDetails = document.querySelectorAll('.job-header + .job-details');
+function toggleDetails(headerSelector, detailsSelector) {
+    const headers = document.querySelectorAll(headerSelector);
+    const details = document.querySelectorAll(detailsSelector);
 
-schoolHeaders.forEach((header, index) => {
-    header.addEventListener('mouseover', function() {
-        jobDetails[index].style.display = 'block';
-    });
-    header.addEventListener('mouseout', function() {
-        jobDetails[index].style.display = 'none';
-    });
-});*/
-
-    /*// Hover to toggle all job details
-    document.querySelectorAll('.job-box').forEach(jobBox => {
-        const jobDetail = jobBox.querySelector('.job-details');
-        
-        jobBox.addEventListener('mouseover', function() {
-            jobDetail.style.display = 'block';
+    headers.forEach((header, index) => {
+        header.addEventListener('mouseover', function() {
+            details[index].style.display = 'block';
         });
-        
-        jobBox.addEventListener('mouseout', function() {
-            jobDetail.style.display = 'none';
+        header.addEventListener('mouseout', function() {
+            details[index].style.display = 'none';
         });
     });
+}
 
-});*/
+toggleDetails('.school-header', '.school-header + .school-details');
+toggleDetails('.job-header', '.job-header + .job-details');
 
 // Function to determine if we're on a mobile device
 function isMobile() {
@@ -92,52 +71,3 @@ function handleEventLogic(headers, details) {
 
 handleEventLogic(document.querySelectorAll('.school-header'), document.querySelectorAll('.school-header + .school-details'));
 handleEventLogic(document.querySelectorAll('.job-header'), document.querySelectorAll('.job-header + .job-details'));
-
-/*// Function to determine if we're on a mobile device
-function isMobile() {
-    return window.innerWidth <= 768;
-}
-
-const schoolHeaders = document.querySelectorAll('.school-header');
-const schoolDetails = document.querySelectorAll('.school-header + .school-details');
-
-schoolHeaders.forEach((header, index) => {
-    if (isMobile()) {
-        header.addEventListener('click', function() {
-            if (schoolDetails[index].style.display === 'block') {
-                schoolDetails[index].style.display = 'none';
-            } else {
-                schoolDetails[index].style.display = 'block';
-            }
-        });
-    } else {
-        header.addEventListener('mouseover', function() {
-            schoolDetails[index].style.display = 'block';
-        });
-        header.addEventListener('mouseout', function() {
-            schoolDetails[index].style.display = 'none';
-        });
-    }
-});
-
-const jobHeaders = document.querySelectorAll('.job-header');
-const jobDetails = document.querySelectorAll('.job-header + .job-details');
-
-jobHeaders.forEach((header, index) => {
-    if (isMobile()) {
-        header.addEventListener('click', function() {
-            if (jobDetails[index].style.display === 'block') {
-                jobDetails[index].style.display = 'none';
-            } else {
-                jobDetails[index].style.display = 'block';
-            }
-        });
-    } else {
-        header.addEventListener('mouseover', function() {
-            jobDetails[index].style.display = 'block';
-        });
-        header.addEventListener('mouseout', function() {
-            jobDetails[index].style.display = 'none';
-        });
-    }
-});*/
