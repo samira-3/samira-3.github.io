26974757f0a426f8557456a8c255a3b88953442f
Samira <95975916+samira-3@users.noreply.github.com>
2023-09-26 03:09:27 -0400

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,43 +1,3 @@
-// JS for nav tab
-// dictates navtab collapse and expand functionality
-const toggleButton = document.getElementsByClassName('index-navtab-toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('index-navtab-navlist')[0]
-
-toggleButton.addEventListener('click', () => {
-  navtabLinks.classList.toggle('active')
-})
-
-const tabs = document.querySelectorAll('.tabs li a');
-const tabContent = document.querySelector('.tab-content');
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', function (e) {
-        // Open a new tab/window when a tab link is clicked
-        window.open('https://samira-3.github.io/experience.html', '_blank');
-
-        // Check if Ctrl or Cmd key is pressed
-        if (e.ctrlKey || e.metaKey) {
-            return; // Do not prevent the default behavior, let the browser open the link in a new tab
-        }
-
-        // If Ctrl or Cmd key is not pressed, continue with the tab-switching logic
-        e.preventDefault();
-        tabs.forEach(t => t.classList.remove('active'));
-        this.classList.add('active');
-        tabContent.innerHTML = `<h2>${this.textContent} Content</h2>`;
-    });
-});
-
-        //e.preventDefault();
-        //tabs.forEach(t => t.classList.remove('active'));
-        //this.classList.add('active');
-
-        // You can load content dynamically for each tab here
-        // For now, let's just update the content to indicate which tab is active
-        //tabContent.innerHTML = `<h2>${this.textContent} Content</h2>`;
-    //});
-//});
-
 document.addEventListener("DOMContentLoaded", function() {
     // Link color change
     document.querySelectorAll('a').forEach(link => {
@@ -50,43 +10,48 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     function closeDetails(element) {
-    element.parentElement.style.display = 'none';
-}
+        element.parentElement.style.display = 'none';
+    }
 
     function handleEventLogic(headersSelector, detailsSelector) {
-    const headers = document.querySelectorAll(headersSelector);
-    const details = document.querySelectorAll(detailsSelector);
+        const headers = document.querySelectorAll(headersSelector);
+        const details = document.querySelectorAll(detailsSelector);
 
-    headers.forEach((header, index) => {
-        header.addEventListener('click', function() {
-            toggleDisplay(details[index]);
+        headers.forEach((header, index) => {
+            header.addEventListener('click', function() {
+                toggleDisplay(details[index]);
+            });
         });
-    });
-}
-    
-    /*/ Generic function to handle hover display for headers and details
-    function handleEventLogic(headersSelector, detailsSelector) {
+    }
+
+    function handleEventLogicWithPopout(headersSelector, detailsSelector) {
         const headers = document.querySelectorAll(headersSelector);
         const details = document.querySelectorAll(detailsSelector);
 
         headers.forEach((header, index) => {
             if (isMobile()) {
                 header.addEventListener('click', function() {
-                    toggleDisplay(details[index]);
+                    toggleDisplayWithPopout(details[index]);
                 });
             } else {
                 header.addEventListener('mouseover', function() {
                     details[index].style.display = 'block';
+                    setTimeout(() => {
+                        details[index].classList.add('active');
+                    }, 10);
                 });
                 header.addEventListener('mouseout', function() {
-                    details[index].style.display = 'none';
+                    details[index].classList.remove('active');
+                    setTimeout(() => {
+                        details[index].style.display = 'none';
+                    }, 500); // Delay for transition to finish before hiding
                 });
             }
         });
-    }*/
+    }
 
     handleEventLogic('.school-header', '.school-header + .school-details');
-    handleEventLogic('.job-header', '.job-header + .job-details');
+    handleEventLogicWithPopout('.job-header', '.job-header + .job-details');
 });
 
 // Function to determine if we're on a mobile device
@@ -96,4 +61,18 @@ function isMobile() {
 
 function toggleDisplay(element) {
     element.style.display = element.style.display === 'block' ? 'none' : 'block';
+}
+
+function toggleDisplayWithPopout(element) {
+    if (element.style.display === 'none' || !element.style.display) {
+        element.style.display = 'block';
+        setTimeout(() => {
+            element.classList.add('active');
+        }, 10); // Delay for display: block to take effect before applying transition
+    } else {
+        element.classList.remove('active');
+        setTimeout(() => {
+            element.style.display = 'none';
+        }, 500); // Delay for transition to finish before hiding
+    }
 }
