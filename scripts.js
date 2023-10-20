2e70d9b6cc921c2acdb2fb373ffa9f535f2dcc07
Samira <95975916+samira-3@users.noreply.github.com>
2023-10-20 17:12:14 -0400

    scripts.js

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-// Function to load content from experience.html
+    // Function to load content from experience.html
     function loadExperience() {
         var container = document.getElementById("experience-container");
         var xhr = new XMLHttpRequest();
@@ -32,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function() {
         loadExperience();
     });
 
-document.addEventListener("DOMContentLoaded", function() {
     // Get the select element by its id
     var select = document.getElementById("role-select");
 
@@ -46,56 +45,55 @@ document.addEventListener("DOMContentLoaded", function() {
             // Redirect to the selected page
             window.location.href = selectedOption;
         }
-    }
-});
-    
-var isGraduated = true; // Change this based on your condition
-
-var badge = document.querySelector('.badge');
-
-if (isGraduated) {
-    badge.classList.add('graduated-badge');
-    badge.querySelector('.badge-text').textContent = 'Graduated';
-} else {
-    badge.classList.add('achieved-badge');
-    badge.querySelector('.badge-text').textContent = 'Achieved';
-}
-
-window.addEventListener('scroll', function () {
-    const cards = document.querySelectorAll('.card'); // Select all cards
-
-    cards.forEach(function (card) {
-        // Calculate the distance between the card and the top of the viewport
-        const distanceToTop = card.getBoundingClientRect().top;
-
-        // Set the card's opacity based on its position
-        card.style.opacity = 1 - distanceToTop / window.innerHeight;
-
-        // Clamp the opacity between 0 and 1 to prevent negative values or values greater than 1
-        card.style.opacity = Math.max(0, card.style.opacity);
-        card.style.opacity = Math.min(1, card.style.opacity);
     });
-});
-
-// Function to determine if we're on a mobile device
-function isMobile() {
-    return window.innerWidth <= 768;
-}
-
-function toggleDisplay(element) {
-    element.style.display = element.style.display === 'block' ? 'none' : 'block';
-}
-
-function toggleDisplayWithPopout(element) {
-    if (element.style.display === 'none' || !element.style.display) {
-        element.style.display = 'block';
-        setTimeout(() => {
-            element.classList.add('active');
-        }, 10); // Delay for display: block to take effect before applying transition
+    
+    var isGraduated = true; // Change this based on your condition
+    
+    var badge = document.querySelector('.badge');
+    
+    if (isGraduated) {
+        badge.classList.add('graduated-badge');
+        badge.querySelector('.badge-text').textContent = 'Graduated';
     } else {
-        element.classList.remove('active');
-        setTimeout(() => {
-            element.style.display = 'none';
-        }, 500); // Delay for transition to finish before hiding
+        badge.classList.add('achieved-badge');
+        badge.querySelector('.badge-text').textContent = 'Achieved';
     }
-}
+    
+    window.addEventListener('scroll', function () {
+        const cards = document.querySelectorAll('.card'); // Select all cards
+    
+        cards.forEach(function (card) {
+            // Calculate the distance between the card and the top of the viewport
+            const distanceToTop = card.getBoundingClientRect().top;
+    
+            // Set the card's opacity based on its position
+            card.style.opacity = 1 - distanceToTop / window.innerHeight;
+    
+            // Clamp the opacity between 0 and 1 to prevent negative values or values greater than 1
+            card.style.opacity = Math.max(0, card.style.opacity);
+            card.style.opacity = Math.min(1, card.style.opacity);
+        });
+    });
+    
+    // Function to determine if we're on a mobile device
+    function isMobile() {
+        return window.innerWidth <= 768;
+    }
+    
+    function toggleDisplay(element) {
+        element.style.display = element.style.display === 'block' ? 'none' : 'block';
+    }
+
+    function toggleDisplayWithPopout(element) {
+        if (element.style.display === 'none' || !element.style.display) {
+            element.style.display = 'block';
+            setTimeout(() => {
+                element.classList.add('active');
+            }, 10); // Delay for display: block to take effect before applying transition
+        } else {
+            element.classList.remove('active');
+            setTimeout(() => {
+                element.style.display = 'none';
+            }, 500); // Delay for transition to finish before hiding
+        }
+    }
